0525f746903418fcb9fd97fe9adc90f42e4355b6
JosePDQ <a23485@alunos.ipca.pt>
2023-10-06 12:25:58 +0100

    Update Script
    hunger/HP

--- Assets/Scipts/UI.cs
+++ Assets/Scipts/UI.cs
@@ -13,7 +13,7 @@ public class timer : MonoBehaviour
     private float displayText;
     private int minutos = 0;
     private int hp = 100;
-    private int hunger = 100;
+    private int hunger = 1;
 
 
     void Update()
@@ -21,13 +21,36 @@ public class timer : MonoBehaviour
         currentTime += Time.deltaTime;
         displayText = Mathf.Round(currentTime);
         displayTime.text = minutos.ToString() + " : " + displayText.ToString();
-        if (currentTime > 60)
+        if (currentTime > 10)
         {
             minutos = minutos + 1;
             currentTime = 0;
+            if (hunger == 0)
+            { hunger = 0; }
+            else hunger -= 1;
+            
         }
-
+        Morte();
         displayHp.text = "HP : " + hp.ToString();
         displayHunger.text = "Hunger : " + hunger.ToString();
+        
+    }
+
+
+    private void Morte()
+    {
+        if (hp == 0)
+        {
+            hp = 0;
+            
+        }
+        else
+        {
+            if (currentTime /2 == 0)
+            {
+                hp -= 1;
+            }
+        }
+        
     }
 }
