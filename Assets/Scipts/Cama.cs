5e093bfdee809f7c6678074528edd8d4b012dc6e
JosePDQ <a23485@alunos.ipca.pt>
2023-10-19 15:51:56 +0100

    Sleep

--- Assets/Scipts/Cama.cs
+++ Assets/Scipts/Cama.cs
@@ -8,44 +8,36 @@ public class Cama : MonoBehaviour
 {
     mycontrolls inputActions;
     public UnityEvent dormir;
+    public bool cama = false;
 
     private void Awake()
     {
         inputActions = new mycontrolls();
     }
+    private void Update()
+    {
+        if (cama == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
 
+                dormir.Invoke();
+                print("dormir");
+                cama = false;
+            }
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
+            cama = true;
             print("cama");
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
             print("saiu");
-    }
-
-    public void OnEnable()
-    {
-        inputActions.Enable();
-    }
-
-    public void OnDisable()
-    {
-        inputActions.Disable();
-        
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            if(inputActions.Player.Interact.WasPressedThisFrame())
-            {
-                dormir.Invoke();    
-                print("Sup");
-            }
-        }
+            cama = false;
     }
 }
