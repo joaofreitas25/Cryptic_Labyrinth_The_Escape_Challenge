ad2310b56e23be95bc094ca9d8146b41e9054de3
Joao Freitas <a23475@alunos.ipca.pt>
2023-11-11 18:47:50 +0000

    north door

---
+++ Assets/OpenDoorNorth.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+
+public class OpenDoorNorth : MonoBehaviour
+{
+
+    private Animator anim;
+
+    private bool IsAtDoor = false;
+
+    [SerializeField] private TextMeshProUGUI CodeText;
+    string codeTextValue = "";
+    public string SafeCode;
+    public GameObject CodePanel;
+    public UnityEvent keypad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CodeText.text = codeTextValue;
+
+        if (codeTextValue == SafeCode)
+        {
+            anim.SetTrigger("OpenDoor");
+            CodePanel.SetActive(false);
+            Time.timeScale = 1;
+            UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+            UnityEngine.Cursor.visible = false;
+        }
+
+        if (codeTextValue.Length >= 7)
+        {
+            codeTextValue = "";
+        }
+
+        //Debug.Log("funciona");
+        if (Input.GetKey(KeyCode.P) && IsAtDoor == true)
+        {
+            Debug.Log("funciona");
+            //CodePanel.SetActive(true);
+            keypad.Invoke();
+            Time.timeScale = 0;
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+            Debug.Log("P");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            IsAtDoor = true;
+            Debug.Log("POrta");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        IsAtDoor = false;
+        CodePanel.SetActive(false);
+        Time.timeScale = 1;
+        UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+        UnityEngine.Cursor.visible = false;
+    }
+
+    public void AddDigit(string digit)
+    {
+        codeTextValue += digit;
+    }
+}
