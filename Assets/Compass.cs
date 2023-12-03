874bfb9a16018464f0fa4fee8af4d8a46d2795f5
Joao Freitas <a23475@alunos.ipca.pt>
2023-12-03 02:00:55 +0000

    Bússola

---
+++ Assets/Compass.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass : MonoBehaviour
+{
+
+    public Transform player;
+    Vector3 vector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vector.z = player.eulerAngles.y;
+        transform.localEulerAngles = vector;
+    }
+}
