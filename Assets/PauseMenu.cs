8fa0244558142cfb3cafd310d216475c0bf892cd
JosePDQ <a23485@alunos.ipca.pt>
2023-12-05 12:12:10 +0000

    game paused

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -7,7 +7,7 @@ public class PauseMenu : MonoBehaviour
     
     public static bool GameIsPaused = false;
 
-    public GameObject pauseMenuUI;
+    //public GameObject pauseMenuUI;
 
     // Update is called once per frame
     public void Update()
@@ -26,14 +26,14 @@ public class PauseMenu : MonoBehaviour
 
     public void Resume () 
     {
-        pauseMenuUI.SetActive(false);
+        //pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
 
     public void Pause ()
     {
-        pauseMenuUI.SetActive(true);
+        //pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
