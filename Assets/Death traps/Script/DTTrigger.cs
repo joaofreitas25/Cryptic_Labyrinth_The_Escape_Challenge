b8efa7de413566208c7f145d1403cebcde5db2b6
SirRick762 <a23998@alunos.ipca.pt>
2023-12-13 17:49:30 +0000

    Puddles+ Sound Effects Traps

--- Assets/Death traps/Script/DTTrigger.cs
+++ Assets/Death traps/Script/DTTrigger.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class DTTrigger : MonoBehaviour
 {
+    public AudioSource audioSource;
     private void OnTriggerEnter(Collider obj)
     {
         GameObject thespikes = GameObject.FindWithTag("spikes");
         thespikes.GetComponent<Animation>().Play("spikes");
+        AudioSource audioSource1 = audioSource;
+        audioSource1.Play();
+
     }
 }
