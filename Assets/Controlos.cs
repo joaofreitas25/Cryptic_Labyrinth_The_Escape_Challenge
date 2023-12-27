d4c405a5c2a7faf81ff02f03b9e1ee213ea5869a
JosePDQ <a23485@alunos.ipca.pt>
2023-12-27 23:38:57 +0000

    pausemenu

--- Assets/Controlos.cs
+++ Assets/Controlos.cs
@@ -1,19 +1,22 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MostrarControles : MonoBehaviour
 {
     public GameObject janelaControles;
+    public UnityEvent controllimg;
 
     void Start()
     {
         // Desativa a janela de controles no início do jogo
-        janelaControles.SetActive(false);
+        //janelaControles.SetActive(false);
     }
 
     public void MostrarOcultarControles()
     {
         // Inverte o estado de ativação da janela de controles
-        janelaControles.SetActive(!janelaControles.activeSelf);
+        //janelaControles.SetActive(!janelaControles.activeSelf);
+        controllimg.Invoke();
     }
 }
 
