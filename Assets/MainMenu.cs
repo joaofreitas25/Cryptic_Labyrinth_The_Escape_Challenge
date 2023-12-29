9a38a1c5e7f1fecb4bd73549ed7a1bea8ec299ee
SirRick762 <a23998@alunos.ipca.pt>
2023-12-29 19:55:14 +0000

    CutScene

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame ()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Cutscene1");
     }
 
     public void QuitGame () 
